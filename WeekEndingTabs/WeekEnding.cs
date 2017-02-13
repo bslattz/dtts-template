2ea6a445eee3036f9e0e397b8c8a97040ea8d574
bslattz <robert_slattery@hotmail.com>
2017-02-13 17:58:47 +1100

    Everything now done with linq

--- WeekEndingTabs/WeekEnding.cs
+++ WeekEndingTabs/WeekEnding.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 using Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Tools;
-using Microsoft.Office.Tools.Excel;
-using Workbook = Microsoft.Office.Tools.Excel.Workbook;
-using WorksheetBase = Microsoft.Office.Tools.Excel.WorksheetBase;
 using Worksheet = Microsoft.Office.Tools.Excel.Worksheet;
 using Excel = Microsoft.Office.Interop.Excel;
 
@@ -20,20 +15,24 @@ namespace WeekEndingTabs
 		public WeekEnding(ThisWorkbook wb)
 		{
 			_wb = wb;
-			var configSheet = (Microsoft.Office.Interop.Excel.Worksheet) wb.Sheets["config"];
+			var configSheet = (Excel.Worksheet) wb.Sheets["config"];
 			if (configSheet == null) return;
 			((DocEvents_Event) configSheet).Calculate += Refresh;
 		}
-
 		public void Refresh()
 		{
+			var updating = _wb.Application.ScreenUpdating;
+			_wb.Application.ScreenUpdating = false;
 			_update();
+			if(updating)
+				_wb.Application.ScreenUpdating = true;
 		}
-
+		private bool _guard;
 		private void _update ()
 		{
-			var daySheets = new SheetsCollection();
-			var sheetDateAddress = _wb.Names.Item("closingDate").RefersToRange.Address;
+			if (_guard) return;
+			var closingDateRange = _wb.Names.Item("closingDate").RefersToRange;
+			var sheetDateAddress = closingDateRange.Address;
 			var days = _wb. Names.Item("daysOfWeek").RefersToRange.ToArray<string>();
 			var dates =_wb. Names.Item("dayDates").RefersToRange.ToArray<DateTime>();
 			var sheetTags = _wb. Names.Item("datedSheets").RefersToRange.ToArray<string>();
@@ -49,118 +48,71 @@ namespace WeekEndingTabs
 
 			// Select sheets to be labeled with date info from all sheets using sheetTags
 			//   then update the Sheet names to be <sheetTag><date info>
-			//   then select the day sheets and
-			//     order them to match the calculated sheetNames range and
-			//     write the new dates in sheetDateAddress
-			sheetTags.Join<string, Worksheet, string, SheetsCollection.Record>(
+			//   and store the results in a dictionary
+			var taggedSheets = sheetTags.Join(
 					allSheetsHosted,
 					tag => tag, sheet => sheet.Name,
-					(tag, sheet) => new SheetsCollection.Record {Key = tag, Sheet = sheet},
-					daySheets.SheetToTagEquivalence)
+					(tag, sheet) => new {Key = tag, Sheet = sheet},
+					_sheetToTagEquivalence)
 				.Select((r, i) =>
 				{
 					r.Sheet.Name = sheetNames[i];
 					return r;
 				})
-				.ToList();
+				.ToDictionary(r => r.Key, r => r.Sheet);
 
-			daySheets
-				.Add(sheetTags.Join<string, Worksheet, string, SheetsCollection.Record>(
-						allSheetsHosted, 
-						name => name, sheet => sheet.Name, 
-						(name, sheet) => new SheetsCollection.Record { Key = name, Sheet = sheet},
-						daySheets.SheetToTagEquivalence)
-					.Select((r, i) => { r.Sheet.Name = sheetNames[i]; return r; })
-					.ToList())
-				.Sort(sheetTags);
+			var actSht = Globals.ThisWorkbook.ActiveSheet as Excel.Worksheet;
 
-			days.Join<string, string, string, Worksheet>(daySheets.Keys,
-				name => name, sheet => sheet,
-				(name, sheet) => daySheets[name],
-				daySheets.SheetToTagEquivalence)
+			// Select the day sheets and
+			//   order them to match the calculated sheetNames range and
+			//   write the new dates in sheetDateAddress
+			foreach (var s in 
+				days.Join<string, string, string, Worksheet>(taggedSheets.Keys,
+					name => name, sheet => sheet,
+					(name, sheet) => taggedSheets[name],
+					_sheetToTagEquivalence)
 				.Select((s, i) =>
 				{
+					if(i > 0)
+						taggedSheets[days[i]].Move(After: taggedSheets[days[i - 1]].InnerObject);
 					s.Range[sheetDateAddress].Value2 = dates[i];
-					return $"{s.Name}\t{dates[i]}\t{s.Range[sheetDateAddress].Value}";
-				}).ToArray();
+					return dates[i];
+				})
+			)
+			// Update the closing date on the summary sheet in case the sheets were re-ordered
+			_guard = true;
+			closingDateRange.Value2 = dates.Last();
+			_guard = false;
+
+			actSht?.Activate();
 		}
-		private static T[] ConvertRange<T>(object[,] r)
+		private readonly IEqualityComparer<object> _sheetToTagEquivalence = new SheetComparer();
+		private class SheetComparer : IEqualityComparer<object>
 		{
-			var l = r.GetUpperBound(1);
-			var ret = new T[l];
-			for (var i = 0; i < l; i++)
+			bool IEqualityComparer<object>.Equals (object sheet, object day)
 			{
-				ret[i] = (T) r[1, i + 1];
+				var sheetName = sheet as string;
+				var dayName = day as string;
+
+				if (dayName == null && sheetName == null)
+					return true;
+				if (dayName == null || sheetName == null)
+					return false;
+
+				bool match = sheetName.IndexOf(dayName, StringComparison.Ordinal) != -1;
+
+				return match;
 			}
-			return ret;
-		}
-		private class SheetsCollection : Dictionary<string, Worksheet>
-		{
-			public SheetsCollection Add (Record record)
+			public int GetHashCode (object obj)
 			{
-				base.Add(record.Key, record.Sheet);
-				return this;
-			}
-			public SheetsCollection Add (List<Record> records)
-			{
-				records.ForEach(r => Add(r));
-				return this;
-			}
-			public SheetsCollection Sort (IReadOnlyList<string> sortOrder, int delay = 0)
-			{
-				var actSht = Globals.ThisWorkbook.ActiveSheet as Excel.Worksheet;
-
-				for (var i = sortOrder.Count - 2; i > -1; i--)
-				{
-					this[sortOrder[i]].Move(Before:this[sortOrder[i+1]].InnerObject);
-					Thread.Sleep(delay);
-				}
-
-				actSht?.Activate();
-
-				return this;
-			}
-
-			public new readonly IEqualityComparer<object> SheetToTagEquivalence = new SheetComparer();
-
-			private class SheetComparer : IEqualityComparer<object>
-			{
-				public new bool Equals(object sheet, object day)
-				{
-					var sheetName = sheet as string;
-					var dayName = day as string;
-
-					Debug.Write($"{day} --> {sheet}\t");
-
-					if (dayName == null && sheetName == null)
-						return true;
-					if (dayName == null || sheetName == null)
-						return false;
-
-					bool match = sheetName.IndexOf(dayName, StringComparison.Ordinal) != -1;
-
-					Debug.WriteLine("{0}", match);
-
-					return match;
-				}
-
-				public int GetHashCode(object obj)
-				{
-					return base.GetHashCode(); // force to use Equals
-				}
-			}
-
-			public class Record
-			{
-				public string Key;
-				public Worksheet Sheet;
+				return base.GetHashCode(); // force to use Equals
 			}
 		}
 	}
 
 	public static class ExcelExtensions
 	{
-		public static T[] ToArray<T> (this Microsoft.Office.Interop.Excel.Range range)
+		public static T[] ToArray<T> (this Range range)
 		{
 			var r = range.Value2;
 			var rows = r.GetUpperBound(0);
