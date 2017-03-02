27d4ee1c559e4ff7c98ab0efe53e5aa5c24de744
bslattz <robert_slattery@hotmail.com>
2017-03-03 00:34:35 +1100

    Made the postAction more testable by adding a Messagebox fake Wrapped the Office versions dictionary to allow for the posability of processing on accessor Added an outer switch to minimise activity on non-install events (postAction is called on every startup of target docs) Decided to always store the template in the User templates location. Refactored registry queries into seperate library Added tets

--- FileCopyPDA/FileCopyPDA.cs
+++ FileCopyPDA/FileCopyPDA.cs
@@ -4,86 +4,172 @@ using Microsoft.VisualStudio.Tools.Applications.Deployment;
 using Microsoft.VisualStudio.Tools.Applications;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Microsoft.Win32;
 using Excel = Microsoft.Office.Interop.Excel;
+using Services = RegistryServices.Service;
 
 namespace FileCopyPDA
 {
     public class FileCopyPDA : IAddInPostDeploymentAction
     {
+        public interface IMessageBox
+        {
+            DialogResult Show (IWin32Window window, string text, string caption, 
+                MessageBoxButtons buttons);
+            DialogResult Show (string text, string caption);
+        }
+        private class MessageBoxClass : IMessageBox
+        {
+            public DialogResult Show(IWin32Window window, string text, string caption,
+                MessageBoxButtons buttons)
+            {
+                SetForegroundWindow(window.Handle);
+                return MessageBox.Show(window, text, caption, buttons);
+            }
+
+            public DialogResult Show(string text, string caption)
+            {
+                return MessageBox.Show(text, caption);
+            }
+        }
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetForegroundWindow (IntPtr hWnd);
+
+        private readonly IMessageBox _messageBox;
+
+        public FileCopyPDA()
+        {
+            _messageBox = new MessageBoxClass();
+        }
+        public FileCopyPDA (IMessageBox messageBox = null)
+        {
+            _messageBox = messageBox ?? new MessageBoxClass();
+        }
         public void Execute(AddInPostDeploymentActionArgs args)
         {
-            const string dataDirectory = @"Data\DTTS TEMPLATE AUTO.XP.xlt";
-            const string file = @"DTTS TEMPLATE AUTO.XP.xlt";
-            var destPath = getTemplatePath();
-            var destFile = System.IO.Path.Combine(destPath, file);
-
             switch (args.InstallationStatus)
             {
                 case AddInInstallationStatus.InitialInstall:
                 case AddInInstallationStatus.Update:
-                    var sourcePath = args.AddInPath;
-                    var deploymentManifestUri = args.ManifestLocation;
-
-                    var officeVersion = QueryRegistry(Registry.ClassesRoot,
-                        @"Excel.Application\CurVer")
-                        .Replace(".0", "").Split('.').Last();
-
-                    var sourceFile = System.IO.Path.Combine(sourcePath, dataDirectory);
-
-                    if (MessageBox.Show(new Form { TopMost = true }, 
-                        $"Add template to {destFile}?\nUpdate status: {args.InstallationStatus.ToString()}" +
-                        $"\nRegistry Templates Path {Environment.GetFolderPath(Environment.SpecialFolder.Templates)}",
-
-                        $"Install DTTS ver {args.Version} for {officeVer[officeVersion]}",
-                        MessageBoxButtons.OKCancel) == DialogResult.Cancel)
-                        return;
-
-                    File.Copy(sourceFile, destFile, true);
-                    ServerDocument.RemoveCustomization(destFile);
-                    ServerDocument.AddCustomization(destFile,
-                        deploymentManifestUri);
-                    break;
                 case AddInInstallationStatus.Uninstall:
-                    if (File.Exists(destFile))
+                    const string dataDirectory =
+                        @"Data\DTTS TEMPLATE AUTO.XP.xlt";
+                    const string file = @"DTTS TEMPLATE AUTO.XP.xlt";
+                    const string appName = "Excel";
+                    var officeVersion =
+                        Services.QueryRegistry(Registry.ClassesRoot,
+                                @"Excel.Application\CurVer")
+                            .Replace(".0", "").Split('.').Last();
+                    var destPath = getTemplatePath($"{officeVersion}.0", appName);
+                    var destFile = System.IO.Path.Combine(destPath, file);
+                    var info = new settingsDictionary(appName);
+                    switch (args.InstallationStatus)
                     {
-                        File.Delete(destFile);
+                        case AddInInstallationStatus.InitialInstall:
+                        case AddInInstallationStatus.Update:
+                            var sourcePath = args.AddInPath;
+                            var deploymentManifestUri = args.ManifestLocation;
+
+
+                            var sourceFile = System.IO.Path.Combine(sourcePath,
+                                dataDirectory);
+
+                            var title =
+                                $"Install DTTS ver {args.Version} for {info[officeVersion]}";
+                            var message =
+                                $"Add template to:\n  {destFile}?\nUpdate status: {args.InstallationStatus.ToString()}";
+                            if (_messageBox.Show(new Form { TopMost = true },
+                                    message, title,
+                                    MessageBoxButtons.OKCancel) ==
+                                DialogResult.Cancel)
+                                return;
+
+                            File.Copy(sourceFile, destFile, true);
+                            ServerDocument.RemoveCustomization(destFile);
+                            ServerDocument.AddCustomization(destFile,
+                                deploymentManifestUri);
+                            break;
+                        case AddInInstallationStatus.Uninstall:
+                            if (File.Exists(destFile))
+                            {
+                                File.Delete(destFile);
+                            }
+                            break;
                     }
+                    break;
+                case AddInInstallationStatus.RunFromCache:
+                    break;
+                case AddInInstallationStatus.RunFromFolder:
+                    break;
+                case AddInInstallationStatus.Offline:
+                    break;
+                case AddInInstallationStatus.Rollback:
                     break;
             }
         }
 
-        public string QueryRegistry(RegistryKey root, string path)
-        {
-            var keys = path.Split(Path.DirectorySeparatorChar);
-            return keys.Aggregate(root, (r, k)  => 
-                r?.OpenSubKey(k)
-            ).GetValue(null).ToString();
-        }
-
-        public string getTemplatePath()
+        public string getTemplatePath(string ver, string appName)
         {
             var app = new Excel.Application { Visible = false };
 
-            var destPath = app.TemplatesPath;
-            if (string.IsNullOrEmpty(destPath))
-                destPath =
-                    Environment.GetFolderPath(Environment.SpecialFolder.Templates);
+            var destPathApp = app.TemplatesPath;
             app.Quit();
-            return destPath;
+
+            var destPath = Services
+                .FindKey(Registry.CurrentUser,
+                    new List<string> {"SOFTWARE", "Microsoft", "Office", ver, appName, "Options"})
+                    .GetValue("PersonalTemplates") as string;
+
+            var message = $"From the App:\n  {destPathApp}" +
+                $"\nFrom the Registry:\n  {destPath}";
+
+            _messageBox.Show(message, "Default User Template Path");
+
+            return destPathApp ?? destPath;
         }
-        private Dictionary<string, string> officeVer = new Dictionary<string, string>
+        class settingsDictionary
         {
-            { "7", "Office 97" },
-            { "8", "Office 98" },
-            { "9", "Office 2000" },
-            { "10", "Office XP" },
-            { "11", "Office 2003" },
-            { "12", "Office 2007" },
-            { "14", "Office 2010" },
-            { "15", "Office 2013" },
-            { "16", "Office 2016" }
-        };
+            public struct OfficeSettings
+            {
+                public string Name;
+                public string PersonalTemplatesRegPath;
+                public string UserTemplatesRegPath;
+
+                private const string path =
+                    @"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Office\{VER}\{APPLICATION}\Options";
+
+                public OfficeSettings (string name, string personal, string user)
+                {
+                    Name = name;
+                    PersonalTemplatesRegPath = personal;
+                    UserTemplatesRegPath = user;
+                }
+            }
+
+            private string _application;
+
+            public settingsDictionary(string application)
+            {
+                _application = application;
+            }
+
+            private string transform (string s, int i) => 
+                s.Replace("{VER}", i.ToString()).Replace("{APPLICATION}", _application);
+            public string this[string key] => _officeVer[key];
+            private readonly Dictionary<string, string> _officeVer = new Dictionary<string, string>
+            {
+                { "7", "Office 97" },
+                { "8", "Office 98" },
+                { "9", "Office 2000" },
+                { "10", "Office XP" },
+                { "11", "Office 2003" },
+                { "12", "Office 2007" },
+                { "14", "Office 2010" },
+                { "15", "Office 2013" },
+                { "16", "Office 2016" }
+            };
+        }
     }
 }
---
+++ FileCopyPDA_Test/message.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.Tools.Applications.Deployment;
+using Microsoft.Win32;
+using IWin32Window = System.Windows.Forms.IWin32Window;
+using Services = RegistryServices.Service;
+
+namespace FileCopyPDA_Test
+{
+    [TestClass]
+    public class RegistryQueryTest
+    {
+        [TestMethod]
+        public void GetTemplatePath_Pass()
+        {
+            const string appName = "Excel";
+            var officeVersion = Services.QueryRegistry(Registry.ClassesRoot,
+                @"Excel.Application\CurVer")
+                .Replace(".0", "").Split('.').Last();
+            var mb = new mockMessageBox();
+            var sut = new FileCopyPDA.FileCopyPDA();
+
+            var templatePath = sut.getTemplatePath($"{officeVersion}.0", appName);
+
+            Assert.IsInstanceOfType(templatePath, typeof(string));
+        }
+        [TestMethod]
+        public void Execute_Pass()
+        {
+            var args = new test_AddInPostDeploymentActionArgs(
+                new Uri("C:\\Users\\Admin\\Dropbox\\Linda\\addin\\Application Files\\WeekEndingTabs_1_0_0_20\\WeekEndingTabs.dll.manifest"), 
+                AddInInstallationStatus.InitialInstall,
+                "",
+                "", "", "", "WeekEndingTabs", "Test Version", ""
+                );
+            var mb = new mockMessageBox();
+            var sut = new FileCopyPDA.FileCopyPDA(mb);
+            sut.Execute(args);
+            Debug.WriteLine(string.Join(("\n"),
+                mb.Trace.Select(o => $"{o.caption}\t{o.text}\t{o.Result}"))
+            );
+            Assert.IsTrue(mb.Trace.Any(m => m.caption.StartsWith("Install DTTS ver")));
+        }
+        public class mockMessageBox : FileCopyPDA.FileCopyPDA.IMessageBox
+        {
+            public struct Message
+            {
+                public string text;
+                public string caption;
+                public DialogResult Result;
+
+                public Message Add(string t, string c, DialogResult r)
+                {
+                    text = t;
+                    caption = c;
+                    Result = r;
+                    return this;
+                }
+            }
+            public List<Message> Trace = new List<Message>();
+            public DialogResult Show (IWin32Window window, string text, string caption,
+                MessageBoxButtons buttons)
+            {
+                var res = MessageBox.Show(window, text, caption, buttons);
+                Trace.Add(new Message().Add(text, caption, res));
+                FileCopyPDA.FileCopyPDA.SetForegroundWindow(window.Handle);
+                return res;               
+            }
+            public DialogResult Show (string text, string caption)
+            {
+                var res = MessageBox.Show(text, caption);
+                Trace.Add(new Message().Add(text, caption, res));
+                return res;
+            }
+        }
+        public class test_AddInPostDeploymentActionArgs :
+            AddInPostDeploymentActionArgs
+        {
+            private List<string> trace { get; } = new List<string>();
+            public test_AddInPostDeploymentActionArgs(
+                Uri manifestLocation, 
+                AddInInstallationStatus installationStatus, 
+                string deploymentManifestXml, 
+                string applicationManifestXml, 
+                string hostManifestXml, 
+                string postActionManifestXml, 
+                string productName, 
+                string version, 
+                string addInPath) : base(manifestLocation, installationStatus, deploymentManifestXml, applicationManifestXml, hostManifestXml, postActionManifestXml, productName, version, addInPath)
+            {}
+        }
+    }
+}
